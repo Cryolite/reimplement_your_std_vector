d8766c997e0d53ffac749a2e1b52168d16971e08
Cryolite <cryolite.indigo@gmail.com>
2018-03-31 07:00:52 +0900

    Reduce the container in size

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-(cd "$THIS_DIR" && cp -r ../test .)
-docker build -t cryolite/reimplement-your-std-vector .
+(cd "$THIS_DIR" && rm -rf test && cp -r ../test .)
+docker build -t cryolite/test-my-vector .
